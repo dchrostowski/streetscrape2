46e6d5dbcff73bb8c9d3d66a1390cde97cc0914c
Dan Chrostowski <dan@danchrostowski.com>
2023-01-07 12:20:47 -0700

    fix setup

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ pip install -r ./requirements.txt
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install 16
 nvm use 16
 cd ./streetscrape/puppeteer
 npm install
