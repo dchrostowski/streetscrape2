f4037bc4009f13d422d25f258b24bc596254c90d
Dan Chrostowski <dan@danchrostowski.com>
2023-01-07 19:38:36 +0000

    fix setup

--- run.sh
+++ run.sh
@@ -5,8 +5,10 @@ cd ./streetscrape/
 sh ./run_swingtradebot.sh
 sh ./run_gurufocus.sh
 cd puppeteer/
+. ~/.profile
 nvm install 16
 nvm use 16
+node --version
 node ./gurufocus.js
 cd ../
 python ingest_gurufocus.py
