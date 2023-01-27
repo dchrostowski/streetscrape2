c8fbebce9940c6e6447cb78f4b5e97744dbec2f4
Dan Chrostowski <dan@danchrostowski.com>
2023-01-26 23:14:05 -0700

    debug middleware

--- streetscrape/streetscrape/settings.py
+++ streetscrape/streetscrape/settings.py
@@ -60,7 +60,7 @@ DOWNLOADER_MIDDLEWARES = {
     #'streetscrape.middlewares.StreetscrapeDownloaderMiddleware': 543,
    'scrapy.downloadermiddlewares.retry.RetryMiddleware': 90,
    'scrapy_proxies.RandomProxy': 100,
-   'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 110,
+   #'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 110,
 
 }
 
