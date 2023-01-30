066a3a3171383bf0d9bc425e80444fe6a9067650
Dan Chrostowski <dan@danchrostowski.com>
2023-01-30 09:22:23 -0700

    cleanup run script

--- run.sh
+++ run.sh
@@ -2,14 +2,13 @@
 
 . ./venv/bin/activate
 cd ./streetscrape/
-sh ./run_swingtradebot.sh
-sh ./run_gurufocus.sh
+scrapy runspider  streetscrape/spiders/swingtradebot.py
+scrapy runspider  streetscrape/spiders/thestreet.py
+scrapy runspider  streetscrape/spiders/zacks.py
+scrapy runspider  streetscrape/spiders/gurufocus.py
 cd puppeteer/
-. ~/.profile
-nvm install 16
 nvm use 16
-node --version
 node ./gurufocus.js
 cd ../
 python ingest_gurufocus.py
-sh ./run_thestreet.sh && sh ./run_zacks.sh
+
