f26aa43401c987ac5d4d9cdec0ed055392cc2e6b
Dan Chrostowski <dan@danchrostowski.com>
2023-02-19 03:44:01 +0000

    fix run.sh for cron

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-#!/bin/sh
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 CWD=$(pwd)
 . ./venv/bin/activate
+[ ! -f "$CWD/streetscrape/twistd.pid"] && sh ./deploy.sh
+
 cd $CWD/streetscrape/
 python api_scheduler.py
+nvm use 16
 python ingest_gurufocus.py
