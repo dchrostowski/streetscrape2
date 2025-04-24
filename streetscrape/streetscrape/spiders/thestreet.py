6f2007b9f4731fcce50fc5f0b7311c43c63c98bb
Dan Chrostowski <dan@danchrostowski.com>
2025-04-23 18:12:04 -0600

    fixed TheStreet spider

--- streetscrape/streetscrape/spiders/thestreet.py
+++ streetscrape/streetscrape/spiders/thestreet.py
@@ -7,57 +7,35 @@ import json
 
 class ThestreetSpider(CrawlSpider):
     name = 'thestreet'
-    allowed_domains = ['www.thestreet.com']
+    allowed_domains = ['www.thestreet.com', 'widgets.tipranks.com']
 
     def start_requests(self):
         pipeline = StreetscrapePipeline()
         queries = pipeline.get_symbols()
+        
         for query in queries:
             (symbol,name) = query
-            url = 'https://api.thestreet.com/marketdata/2/1?includePartnerContent=true&includeLatestNews=false&start=0&rt=true&max=10&filterContent=false&format=json&s=%s&includePartnerNews=false' % symbol
+            url = "https://widgets.tipranks.com/api/etoro/dataForTicker?ticker=%s" % symbol
+            print("getting %s" % url)
             yield scrapy.Request(url=url,callback=self.parse)
 
 
     def calculate_quant_rating(self,grade):
-        if grade is None:
-            return 0
-        rating_map = {
-            'E+': 0,
-            'D-': 1,
-            'D': 2,
-            'D+': 3,
-            'C-': 4,
-            'C': 5,
-            'C+': 6,
-            'B-': 7,
-            'B': 8,
-            'B+': 9,
-            'A-': 10,
-            'A': 11,
-            'A+': 12
-        }
-        number_grade = rating_map[grade]
-        return str(round(number_grade/12 * 100,2))
-
+        return int(grade) * 10
+    
     def parse(self, response):
-        data = json.loads(response.text)
         try:
-            quotes = json.loads(response.text)['response']['quotes']
-            for quote in quotes:
-                item = TheStreetItem()
-                company_name = quote['companyName'].replace(',','')
-                symbol = quote['symbol']
-                grade = quote['letterGradeRating']
-                price = quote['currentPrice']
-                quant_rating = self.calculate_quant_rating(grade)
-
-                item['symbol'] = symbol
-                item['grade'] = grade
-                item['price_at_rating'] = price
-                item['quant'] = quant_rating
-
-                yield item
+            data = json.loads(response.text)['overview']
+            grade = "%s" % data['tipranksStockScore']['score']
+            price_at_rating = data['prices'][-1]['p']
+            symbol = data['ticker']
+            quant = self.calculate_quant_rating(grade)
+            item = TheStreetItem()
+            item['symbol'] = symbol
+            item['grade'] = grade
+            item['price_at_rating'] = price_at_rating
+            item['quant'] = quant
+            print(item)
+            yield item
         except Exception as e:
             print(e)
-
-
